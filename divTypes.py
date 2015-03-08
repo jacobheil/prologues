fbe1c42a2b643ebfc705be096ca9cd1b50791aa2
Jacob Heil <jheil@wooster.edu>
2015-03-08 12:21:12 -0400

    testing readline w asterisk

--- divTypes.py
+++ divTypes.py
@@ -1,6 +1,12 @@
+'''
 import glob
 
 for i in glob.glob("_material_before_act_one/*_before_act_one.xml"):
 	f = open(i)
+
 	print(f.readline())
 	f.close()
+	'''
+f = open('A00960_before_act_one.xml')
+print(f.readline(*))
+f.close()

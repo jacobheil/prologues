f76dfefcc96cf7f4e47475675c31390fc8ca33fc
Jacob Heil <jheil@wooster.edu>
2015-03-08 13:37:53 -0400

    codecs, locating lines

--- divTypes.py
+++ divTypes.py
@@ -7,6 +7,17 @@ for i in glob.glob("_material_before_act_one/*_before_act_one.xml"):
 	print(f.readline())
 	f.close()
 	'''
-f = open('_material_before_act_one/A00960_before_act_one.xml')
+import codecs
+
+with codecs.open('_material_before_act_one/A00725_before_act_one.xml',"r","utf-8") as play:
+	play = play.read()
+	if '<div type="to_the_reader">' in play:
+		print "hooray"
+
+
+'''
+f = open('_material_before_act_one/A00725_before_act_one.xml')
 for line in f:
-	print line
+		print line
+
+'''
